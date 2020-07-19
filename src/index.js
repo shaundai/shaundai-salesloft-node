57e3925f5ff0f968258fb45f75a7102f3d6c38a6
Shaundai Person <shaundaiperson@gmail.com>
2020-07-19 00:14:03 -0400

    delete unnecessary lines and files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 require("dotenv").config();
 const port = process.env.PORT || 3001;
-const salesloftApiKey = process.env.SALESLOFT_API_KEY;
 const salesloftSecret = process.env.SALESLOFT_APP_SECRET;
 const salesloftClientId = process.env.SALESLOFT_APP_ID;
 const cors = require("cors");
 
-const salesloftApi = require('./salesloftApi');
-const accounts = require('./routes/accounts')
-
 const express = require('express');
-const path = require("path");
 const app = express();
 const router = express.Router();
 const axios = require('axios');
@@ -19,14 +14,11 @@ const tokens = {}
 const userInfo = {}
 
 app.get('/', (req, res) => {
-   res.sendFile(__dirname + '../public/App.js')
+   res.send('main app')
 })
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(bodyParser.json())
 app.use(cors());
-
-app.use('/accounts', accounts);
 
 app.post('/token', (req, res) => {
     tokens.accessToken = req.body.accessToken
@@ -110,27 +102,6 @@ app.get('/salesloft', (req, res) => {
     })
 }
 )
-
-const getRefreshToken = (refreshToken) => {
-    return axios({
-            method: 'post',
-            url: `https://accounts.salesloft.com/oauth/token`,
-            params: {
-            "client_id": salesloftClientId,
-            "client_secret": salesloftSecret,
-            "grant_type": "refresh_token",
-            "refresh_token": refreshToken
-            },
-        }).then((response) => {
-            const accessToken = response.data.access_token
-            return accessToken;
-        })
-        .catch((err) => {
-            res.status(404).send(err)
-        })
-    
-        }
-
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
